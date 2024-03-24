7f0eaaae20ef797f7b56e2d33a5557edc2d19abf
cristic1988 <cristic8@gmail.com>
2024-03-25 01:06:34 +0200

    second commit

--- src/Burger.java
+++ src/Burger.java
@@ -5,16 +5,12 @@ public class Burger {
     public String cheese;
     public String greens;
     public String mayonnaise;
+    public int meatCount;
+
+    public Burger() {
 
 
-
-    public Burger (String bun, String meat, String cheese, String greens) {
-        this.bun = bun;
-        this.meat = meat;
-        this.cheese = cheese;
-        this.greens = greens;
     }
-
 
     @Override
     public String toString() {
@@ -24,19 +20,43 @@ public class Burger {
                 ", cheese='" + cheese + '\'' +
                 ", greens='" + greens + '\'' +
                 ", mayonnaise='" + mayonnaise + '\'' +
+                ", meatCount=" + meatCount +
                 '}';
     }
 
-    public Burger ordinaryBurger (String bun, String meat, String cheese, String greens, String mayonnaise) {
+
+    public Burger(String bun, String meat, String cheese, String greens, String mayonnaise, int meatCount) {
         this.bun = bun;
         this.meat = meat;
         this.cheese = cheese;
         this.greens = greens;
         this.mayonnaise = mayonnaise;
-        return null;
+        if (meatCount == 2) {
 
+        } else {
+            System.out.println("Склад бургера з подвійним м'ясом: " + bun + ", " + meat + ", " + cheese + ", " + greens + ", " + mayonnaise);
+
+        }
+    }
+
+    public Burger(String bun, String meat, String cheese, String greens) {
+        this.bun = bun;
+        this.meat = meat;
+        this.cheese = cheese;
+        this.greens = greens;
+        System.out.println("Склад дієтичного бургера: " + bun + ", " + meat + ", " + cheese + ", " + greens);
 
     }
 
+
+    public Burger(String bun, String meat, String cheese, String greens, String mayonnaise) {
+        this.bun = bun;
+        this.meat = meat;
+        this.cheese = cheese;
+        this.greens = greens;
+        this.mayonnaise = mayonnaise;
+        System.out.println("Склад звичайного бургера: " + bun + ", " + meat + ", " + cheese + ", " + greens + ", " + mayonnaise);
+
+    }
 
 }
--- src/BurgerMain.java
+++ src/BurgerMain.java
@@ -1,8 +1,16 @@
 public class BurgerMain {
 
     public static void main (String[] args){
-        Burger ordinaryBurger = new Burger();
-        System.out.println(ordinaryBurger);
+       Burger burger = new Burger();
+       Burger burger1 = new Burger(burger.bun, burger.meat, burger.cheese, burger.greens, burger.mayonnaise);
+       Burger burger2 = new Burger(burger1.bun, burger1.meat, burger1.cheese, burger1.greens);
+       Burger burger3 = new Burger(burger2.bun, burger2.meat, burger2.cheese, burger2.greens, burger2.mayonnaise, burger2.meatCount);
+
+
+
+
+
+
 
     }
 }
